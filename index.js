f07f7dbe51368edc2eac27f55bd3861b2386c943
Wang-Zhongwei <873994270@qq.com>
2022-09-10 09:45:10 -0400

    add date validation

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ User = mongoose.model("User", new mongoose.Schema({ username: String }));
 Exercise = mongoose.model(
   "Exercise",
   new mongoose.Schema({
+    username: String,
     userId: String,
     description: String,
     duration: Number,
@@ -52,6 +53,7 @@ app.post("/api/users/:_id/exercises", (req, res) => {
 
     // if found existing user, create new exercise for the user
     let newExercise = new Exercise({
+      username: user.username,
       userId: userId,
       description: description,
       duration: duration,
@@ -80,26 +82,19 @@ app.get("/api/users", (req, res) => {
   });
 });
 
-// get all exercises for a user
+// get all exercises for a specific user
 app.get("/api/users/:_id/logs", (req, res) => {
   let userId = req.params._id;
-  let from = req.query.from;
-  let to = req.query.to;
+  let from = new Date(req.query.from);
+  let to = new Date(req.query.to);
   let limit = req.query.limit;
 
-  // Parse date
-  if (from === undefined) {
-    from = new Date(0);
-  } else {
-    from = new Date(from);
-  }
-
-  if (to === undefined) {
-    to = new Date();
-  } else {
-    to = new Date(to);
-  }
-
+  // check date
+  if (from.toDateString() === 'Invalid Date') 
+    from = new Date(0); // 1970 Jan 1
+  if (to.toDateString() === 'Invalid Date') 
+    to = new Date(); // now
+  
   User.findById(userId, (err, user) => {
     if (err) return console.error(err);
 
@@ -115,12 +110,14 @@ app.get("/api/users/:_id/logs", (req, res) => {
             duration: exercise.duration,
             date: exercise.date.toDateString(),
           };
-        });
+        })
+        .slice(0, limit === undefined ? exercises.length : limit);
+
       res.json({
-        _id: user._id,
         username: user.username,
         count: log.length,
-        log: log.slice(0, limit === undefined ? log.length : limit),
+        _id: user._id,
+        log: log,
       });
     });
   });
